685e36d7b1c6ccf55e7584d3a1570693f11e87a1
Dmitry <javaist@xsd.ru>
2020-09-14 14:54:44 +0300

    Stage 3 of 5: Life goes by

--- Game of Life/task/src/life/Main.java
+++ Game of Life/task/src/life/Main.java
@@ -1,29 +1,50 @@
 package life;
 
+import java.io.IOException;
 import java.util.Random;
 import java.util.Scanner;
+import java.util.function.DoubleBinaryOperator;
 
 public class Main {
     public static char[][] current;
     public static char[][] next;
     public static int border;
     public static Random random;
+    static final char inhabited = 'O';
+    static final char empty = ' ';
+    static int population = 0;
+    static int generation = 0;
 
-    public static void main(String[] args) {
+
+    public static void main(String[] args) throws InterruptedException {
         Scanner scanner = new Scanner(System.in);
         border = scanner.nextInt();
-        long seed = scanner.nextLong();
-        int eons = scanner.nextInt();
+//        long seed = scanner.nextLong();
+//        int eons = scanner.nextInt();
 
-        random = new Random(seed);
+        random = new Random();
         // world creation function
         generate();
-
-        for (int i = 0; i < eons; i++) {
-            evolve();
-        }
-
         print();
+        Thread.sleep(1000);
+        clear();
+
+        for (int i = 0; i < 10; i++) {
+            evolve();
+            print();
+            Thread.sleep(1000L);
+            clear();
+        }
+    }
+
+    private static void clear() {
+        try {
+            if (System.getProperty("os.name").contains("Windows"))
+                new ProcessBuilder("cmd","/c","cls").inheritIO().start().waitFor();
+            else
+                Runtime.getRuntime().exec("clear");
+        }
+        catch (InterruptedException | IOException e) {}
     }
 
     public static int leap(int direction) {
@@ -48,28 +69,33 @@ public class Main {
                 south = leap(i + 1);
                 west = leap(j - 1);
                 east = leap(j + 1);
-                counter = current[north][j] == 'O' ? counter + 1 : counter;
-                counter = current[north][west] == 'O' ? counter + 1 : counter;
-                counter = current[north][east] == 'O' ? counter + 1 : counter;
-                counter = current[i][west] == 'O' ? counter + 1 : counter;
-                counter = current[i][east] == 'O' ? counter + 1 : counter;
-                counter = current[south][j] == 'O' ? counter + 1 : counter;
-                counter = current[south][west] == 'O' ? counter + 1 : counter;
-                counter = current[south][east] == 'O' ? counter + 1 : counter;
+                counter = current[north][j] == inhabited ? counter + 1 : counter;
+                counter = current[north][west] == inhabited ? counter + 1 : counter;
+                counter = current[north][east] == inhabited ? counter + 1 : counter;
+                counter = current[i][west] == inhabited ? counter + 1 : counter;
+                counter = current[i][east] == inhabited ? counter + 1 : counter;
+                counter = current[south][j] == inhabited ? counter + 1 : counter;
+                counter = current[south][west] == inhabited ? counter + 1 : counter;
+                counter = current[south][east] == inhabited ? counter + 1 : counter;
                 if (counter == 3) {
-                    next[i][j] = 'O';
+                    next[i][j] = inhabited;
+                    population = current[i][j] == inhabited ? population : population + 1;
                 } else if (counter < 2 || counter > 3) {
-                    next[i][j] = ' ';
+                    next[i][j] = empty;
+                    population = current[i][j] == inhabited ? population - 1 : population;
                 } else {
                     next[i][j] = current[i][j];
                 }
             }
         }
+//        population = 0;
         for (int i = 0; i < border; i++) {
             for (int j = 0; j < border; j++) {
                 current[i][j] = next[i][j];
+//                population = current[i][j] == inhabited ? population + 1 : population;
             }
         }
+        generation += 1;
     }
 
     public static void generate() {
@@ -80,17 +106,29 @@ public class Main {
         for (int i = 0; i < border; i++) {
             for (int j = 0; j < border; j++) {
                 alive = random.nextBoolean();
-                current[i][j] = alive ? 'O' : ' ';
+                current[i][j] = alive ? inhabited : empty;
+                population = alive ? population + 1 : population;
             }
         }
+        generation += 1;
     }
 
     public static void print() {
+        StringBuilder string = new StringBuilder();
+        string.append("Generation #");
+        string.append(generation);
+        string.append('\n');
+        string.append("Alive: ");
+        string.append(population);
+        string.append("\n\n");
         for (int i = 0; i < border; i++) {
             for (int j = 0; j < border; j++) {
-                System.out.print(current[i][j]);
+                string.append(current[i][j]);
+//                System.out.print(current[i][j]);
             }
-            System.out.println();
+//            System.out.println();
+            string.append('\n');
         }
+        System.out.println(string);
     }
 }
